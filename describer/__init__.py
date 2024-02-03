dc730b9cea7466ee29521fe203eeac7d267e9ba5
Pavel Cherenkov <the.pavel.cherenkov@gmail.com>
2024-02-03 17:39:39 +0100

    Add kafka reader to describer

--- describer/__init__.py
+++ describer/__init__.py
@@ -1,7 +1,13 @@
-import requests
+from dataclasses import dataclass
+import json
+from time import sleep
+
 import click
+from kafka import KafkaConsumer
 from PIL import Image
+import requests
 from transformers import BlipProcessor, BlipForConditionalGeneration
+import yaml
 
 
 class Model:
@@ -19,18 +25,54 @@ def fetch_image(url: str) -> Image.Image:
     return Image.open(requests.get(url, stream=True).raw).convert('RGB')
 
 
-@click.command()
-@click.option('--model-path', default="./blip-image-captioning-large", help='Path to model')
+@dataclass
+class Context:
+    """Class for keeping track of an item in inventory."""
+    config: dict
+    model: Model
+
+
+@click.group()
+@click.option('--config-path', default='./config.yaml', help='Config path')
+@click.pass_context
+def cli(ctx, config_path):
+    with open(config_path) as f:
+        config = yaml.safe_load(f)
+
+    ctx.ensure_object(dict)
+    ctx.obj = Context(config, Model(config["model_path"]))
+
+
+@cli.command()
 @click.option('--image-url', default='https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg', help='Image url')
-def caption(model_path, image_url):
+@click.pass_context
+def caption(ctx, image_url):
     """Caption the image at the url"""
     raw_image = fetch_image(image_url)
 
-    model = Model(model_path)
-
-    result = model.caption(raw_image)
+    result = ctx.obj.model.caption(raw_image)
 
     print(result)
 
+
+@cli.command()
+@click.pass_context
+def run(ctx):
+    """Read messages from Kafka and describe urls"""
+    consumer = KafkaConsumer(
+        *ctx.obj.config["topics"],
+        **ctx.obj.config["kafka_config"],
+    )
+
+    for msg in consumer:
+        image_url = json.loads(msg)["url"]
+        raw_image = fetch_image(image_url)
+        result = ctx.obj.model.caption(raw_image)
+        print(result)
+
+    if consumer is not None:
+        consumer.close()
+
+
 if __name__ == '__main__':
-    caption()
+    cli()
