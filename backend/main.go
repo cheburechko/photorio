f1f2a734109cfccbb928b12a898321eade436894
Pavel Cherenkov <the.pavel.cherenkov@gmail.com>
2024-02-10 12:09:58 +0100

    Add configuration

--- backend/main.go
+++ backend/main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"bytes"
+	"flag"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"os"
+	"path/filepath"
 	"strings"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type (
+	Config struct {
+		Port         string `yaml:"port" env:"PORT" env-default:"9000"`
+		TemplateRoot string `yaml:"template_root"`
+	}
+
 	App struct {
+		Config         *Config
 		SearchTemplate *template.Template
 	}
 
@@ -19,24 +29,38 @@ type (
 	}
 )
 
-func NewApp() (*App, error) {
-	tmplText, err := os.ReadFile("templates/search_result.html")
-	if err != nil {
-		return nil, err
-	}
-
-	tmpl, err := template.New("search_results").Parse(string(tmplText))
+func NewApp(c *Config) (*App, error) {
+	tmpl, err := template.ParseFiles(filepath.Join(c.TemplateRoot, "search_result.html"))
 	if err != nil {
 		return nil, err
 	}
 
 	return &App{
 		SearchTemplate: tmpl,
+		Config: c,
 	}, nil
 }
 
+func readConfig() (*Config, error) {
+	var cfg Config
+
+	configPath := flag.String("config", "config.yaml", "Path to config")
+
+	flag.Parse()
+
+	err := cleanenv.ReadConfig(*configPath, &cfg)
+
+	return &cfg, err
+}
+
 func main() {
-	app, err := NewApp()
+	cfg, err := readConfig()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	app, err := NewApp(cfg)
 
 	if err != nil {
 		log.Fatal(err)
@@ -48,12 +72,15 @@ func main() {
 	mux.Handle("/", http.HandlerFunc(app.Home))
 	mux.Handle("/search", http.HandlerFunc(app.Search))
 
-	err = http.ListenAndServe(":9000", mux)
+	addr := fmt.Sprintf(":%s", app.Config.Port)
+	log.Printf("Address: %s", addr)
+
+	err = http.ListenAndServe(addr, mux)
 	log.Fatal(err)
 }
 
 func (a *App) Home(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "templates/index.html")
+	http.ServeFile(w, r, filepath.Join(a.Config.TemplateRoot, "index.html"))
 }
 
 func (a *App) Search(w http.ResponseWriter, r *http.Request) {
